dac3a56ae84710e13d19b9379e86d14a1c4817cc
cl-aip <koide@ontolonomy.co.jp>
2015-04-26 16:09:08 +0900

    Mininum modification on GPS program from PAIP

---
+++ gpsaux.lisp
@@ -0,0 +1,70 @@
+;;; -*- Mode: Lisp; Syntax: Common-Lisp -*-
+;;; Code from Paradigms of AI Programming
+;;; Copyright (c) 1991 Peter Norvig
+
+;;; File gpsaux.lisp is a file dedicated to only gps programs.
+;;; Load this file before running any other gps programs.
+
+;;;; Auxiliary Functions
+
+(setf (symbol-function 'find-all-if) #'remove-if-not)
+
+(defun find-all (item sequence &rest keyword-args
+                 &key (test #'eql) test-not &allow-other-keys)
+  "Find all those elements of sequence that match item,
+  according to the keywords.  Doesn't alter sequence."
+  (if test-not
+      (apply #'remove item sequence 
+             :test-not (complement test-not) keyword-args)
+      (apply #'remove item sequence
+             :test (complement test) keyword-args)))
+
+;;; ==============================
+
+(defun mappend (fn list)
+  "Append the results of calling fn on each element of list.
+  Like mapcon, but uses append instead of nconc."
+  (apply #'append (mapcar fn list)))
+
+(defun mklist (x) 
+  "If x is a list return it, otherwise return the list of x"
+  (if (listp x) x (list x)))
+
+(defun flatten (exp)
+  "Get rid of imbedded lists (to one level only)."
+  (mappend #'mklist exp))
+
+(defun random-elt (seq) 
+  "Pick a random element out of a sequence."
+  (elt seq (random (length seq))))
+
+;;; ==============================
+
+;;;; The Debugging Output Facility:
+
+(defvar *dbg-ids* nil "Identifiers used by dbg")
+
+(defun dbg (id format-string &rest args)
+  "Print debugging info if (DEBUG ID) has been specified."
+  (when (member id *dbg-ids*)
+    (fresh-line *debug-io*)
+    (apply #'format *debug-io* format-string args)))
+#+:never
+(defun debug (&rest ids)
+  "Start dbg output on the given ids."
+  (setf *dbg-ids* (union ids *dbg-ids*)))
+
+(defun undebug (&rest ids)
+  "Stop dbg on the ids.  With no ids, stop dbg altogether."
+  (setf *dbg-ids* (if (null ids) nil
+                      (set-difference *dbg-ids* ids))))
+
+;;; ==============================
+
+(defun dbg-indent (id indent format-string &rest args)
+  "Print indented debugging info if (DEBUG ID) has been specified."
+  (when (member id *dbg-ids*)
+    (fresh-line *debug-io*)
+    (dotimes (i indent) (princ "  " *debug-io*))
+    (apply #'format *debug-io* format-string args)))
+
